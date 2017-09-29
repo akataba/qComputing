f3e759baef84e86d392e6bfa9fd7744668205862
Amara Katabarwa <Henriamaa@gmail.com>
2017-09-28 22:14:56 -0400

    Nothing much

--- NoisyEvolution.py
+++ NoisyEvolution.py
@@ -1,9 +1,11 @@
 import Gates as g
 import Operations as op
 import numpy as np
+from decimal import *
+getcontext().prec = 12
 
 
-def decohere(K,rho,n):
+def decohere(K, rho, n):
     """
     Carries out the kraus evolution of density matrix rho
     :param K: An array containing the Kraus Matrices
@@ -12,13 +14,13 @@ def decohere(K,rho,n):
     :return: The evolved density matrix
     """
 
-    out = np.zeros((pow(2,n),pow(2,n)),dtype=complex)
+    out = np.zeros((pow(2, n), pow(2, n)), dtype=complex)
     for x in range(len(K)):
-        out += np.dot(K[x],np.dot(rho,op.ctranspose(K[x])))
+        out += np.dot(K[x], np.dot(rho, op.ctranspose(K[x])))
     return out
 
 
-def kraus_pta(n,t,t1,t2):
+def kraus_pta(n, t, t1, t2):
     """
     Produces the kraus matrices for the pta channel
     :param n: number of qubit
@@ -27,32 +29,33 @@ def kraus_pta(n,t,t1,t2):
     :param t2: dephasing time
     :return: returns a 3 dimensinal array of pta kraus matrices
     """
-    gamma = 1- np.exp(-t/t1)
-    t_phi = 1/t2 - 1/(2*t1)
-    #lambda1 = np.exp(-t/t1)*(1-np.exp(-2*(t/t_phi)))
-    px=py = gamma/4.0
-    pz= 1.0/2.0 - py - np.exp(-t/(2*t1))*np.exp(-(t/t_phi))/2
-    pi =1 -(px+py+pz)
-    print ('px: ',py, 'pz: ',pz, 'pi: ',pi)
-    A = np.zeros((pow(4,n),pow(2,n),pow(2,n)),dtype = complex)  # 3 dimensional array to store kraus matrices
-    ptaOperators={
-       '0': np.sqrt(pi)*g.id(),
-       '1': np.sqrt(px)*g.x(),
-       '2': np.sqrt(py)*g.y(),
-       '3': np.sqrt(pz)*g.z()
+    gamma = 1 - np.exp(-t / t1)
+    t_phi = Decimal(1 / t2) - Decimal(1 / (2 * t1))
+    # lambda1 = np.exp(-t/t1)*(1-np.exp(-2*(t/t_phi)))
+    px = py = gamma / 4.0
+    pz = 1.0 / 2.0 - py - np.exp(-t / (2 * t1)) * np.exp(-(t / t_phi)) / 2
+    pi = 1 - (px + py + pz)
+    print('px: ', py, 'pz: ', pz, 'pi: ', pi)
+    A = np.zeros((pow(4, n), pow(2, n), pow(2, n)), dtype=complex)  # 3 dimensional array to store kraus matrices
+    ptaOperators = {
+        '0': np.sqrt(pi) * g.id(),
+        '1': np.sqrt(px) * g.x(),
+        '2': np.sqrt(py) * g.y(),
+        '3': np.sqrt(pz) * g.z()
     }
 
     # get labels
-    labels = op.createlabel(n,4)
+    labels = op.createlabel(n, 4)
 
     for i in range(len(labels)):
-        temp =1
+        temp = 1
         for digit in labels[i]:
-             temp = np.kron(temp,ptaOperators[digit])
+            temp = np.kron(temp, ptaOperators[digit])
         A[i] = temp
     return A
 
-def pta_ad(n,t,t1):
+
+def pta_ad(n, t, t1):
     """
     Produces the kraus matrices for the pta channel
     :param n: number of qubit
@@ -60,30 +63,30 @@ def pta_ad(n,t,t1):
     :param t1: relaxation time
     :return: returns a 3 dimensinal array of pta kraus matrices
     """
-    gamma = 1- np.exp(-t/t1)
-    px=py = gamma/4.0
-    pz= 1.0/2.0 - py - np.sqrt(1-gamma)/2
-    pi =1 -(px+py+pz)
-    print ('px: ',py, 'pz: ',pz, 'pi: ',pi)
-    A = np.zeros((pow(4,n),pow(2,n),pow(2,n)),dtype = complex)  # 3 dimensional array to store kraus matrices
-    ptaOperators={
-       '0': np.sqrt(pi)*g.id(),
-       '1': np.sqrt(px)*g.x(),
-       '2': np.sqrt(py)*g.y(),
-       '3': np.sqrt(pz)*g.z()
+    gamma = 1 - np.exp(-t / t1)
+    px = py = gamma / 4.0
+    pz = 1.0 / 2.0 - py - np.sqrt(1 - gamma) / 2
+    pi = 1 - (px + py + pz)
+    A = np.zeros((pow(4, n), pow(2, n), pow(2, n)), dtype=complex)  # 3 dimensional array to store kraus matrices
+    ptaOperators = {
+        '0': np.sqrt(pi) * g.id(),
+        '1': np.sqrt(px) * g.x(),
+        '2': np.sqrt(py) * g.y(),
+        '3': np.sqrt(pz) * g.z()
     }
 
     # get labels
-    labels = op.createlabel(n,4)
+    labels = op.createlabel(n, 4)
 
     for i in range(len(labels)):
-        temp =1
+        temp = 1
         for digit in labels[i]:
-             temp = np.kron(temp,ptaOperators[digit])
+            temp = np.kron(temp, ptaOperators[digit])
         A[i] = temp
     return A
 
-def kraus_ad(n,t,t1):
+
+def kraus_ad(n, t, t1):
     """
       Produces the kraus matrices for the amplitude damping channel
         :param n: number of qubit
@@ -92,40 +95,42 @@ def kraus_ad(n,t,t1):
         :return: returns a 3 dimensinal array of amplitude damping kraus matrices
       """
 
-    A = np.zeros((pow(2, n), pow(2, n), pow(2, n)),dtype= complex)  # 3 dimensional array to store kraus matrices
+    A = np.zeros((pow(2, n), pow(2, n), pow(2, n)), dtype=complex)  # 3 dimensional array to store kraus matrices
     gamma = 1 - np.exp(-t / t1)
     adOperators = {
-        "0" : np.array([[1,0],[0, np.sqrt(1-gamma)]]),
-        "1" : np.array([[0,np.sqrt(gamma)],[0, 0]])
+        "0": np.array([[1, 0], [0, np.sqrt(1 - gamma)]]),
+        "1": np.array([[0, np.sqrt(gamma)], [0, 0]])
     }
 
     labels = op.createlabel(n, 2)
 
     for i in range(len(labels)):
-        temp =1
+        temp = 1
         for digit in labels[i]:
-            temp = np.kron(temp,adOperators[digit])
+            temp = np.kron(temp, adOperators[digit])
         A[i] = temp
     return A
 
-def kraus_exact(n,t,t1,t2):
+
+def kraus_exact(n, t, t1, t2):
     """
     Produces the kraus matrices for the exact evolution of amplitude damping with dephasing channel
         :param n: number of qubit
         :param t: time step for evolution
         :param t1: relaxation time
-        :param t2: dephasing time
+        :param t2: dephasing time must be smaller than t1
         :return:  a 3 dimensinal array of kraus matrices with amplitude damping and dephasing
 
     """
-    A = np.zeros((pow(3, n), pow(2, n), pow(2, n)),dtype =complex)  # 3 dimensional array to store kraus matrices
+
+    A = np.zeros((pow(3, n), pow(2, n), pow(2, n)), dtype=complex)  # 3 dimensional array to store kraus matrices
     gamma = 1 - np.exp(-t / t1)
     t_phi = 1 / t2 - 1 / (2 * t1)
-    lambda1 = np.exp(-t / t1)*(1 - np.exp(-2 * (t / t_phi)))
+    lambda1 = np.exp(-t / t1) * (1 - np.exp(-2 * (t / t_phi)))
     krausOperators = {
-        "0" : np.array([[1, 0], [0, np.sqrt(1 -gamma-lambda1)]]),
-        "1" : np.array([[0, np.sqrt(gamma)], [0, 0]]),
-        "2" : np.array([[0, 0], [0, np.sqrt(lambda1)]]),
+        "0": np.array([[1, 0], [0, np.sqrt(1 - gamma - lambda1)]]),
+        "1": np.array([[0, np.sqrt(gamma)], [0, 0]]),
+        "2": np.array([[0, 0], [0, np.sqrt(lambda1)]]),
     }
 
     labels = op.createlabel(n, 3)
@@ -134,5 +139,12 @@ def kraus_exact(n,t,t1,t2):
         temp = 1
         for digit in labels[i]:
             temp = np.kron(temp, krausOperators[digit])
-        A[i] =temp
+        A[i] = temp
     return A
+
+
+def ad_lindbladoperator(n):
+    """
+    :param n: The number of qubits in the system experiencing amplitude damping
+    :return: Returns a list of such operators
+    """
